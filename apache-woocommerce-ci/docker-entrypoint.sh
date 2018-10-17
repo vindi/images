0c51fb9cfd447dedc1e0fc81d0c7f30f29b4344c
Rubens S. Takauti <rtakauti@hotmail.com>
2018-10-17 15:38:05 -0300

    Ajusta download do GitHub

--- apache-woocommerce-ci/docker-entrypoint.sh
+++ apache-woocommerce-ci/docker-entrypoint.sh
@@ -3,9 +3,9 @@
 set -e
 
 /wait-for-it.sh --timeout=5 wordpress_db:3306 --timeout=5 -- echo "mysql is loaded"
-wget https://github.com/vindi/vindi-woocommerce-subscriptions/archive/develop.zip
-wp plugin install ./develop.zip --activate --allow-root
-rm develop.zip
+curl https://github.com/vindi/vindi-woocommerce-subscriptions/archive/develop.zip > vindi-woocommerce-subscriptions.zip
+wp plugin install ./vindi-woocommerce-subscriptions.zip --activate --allow-root
+rm vindi-woocommerce-subscriptions.zip
 wp plugin update --all --allow-root
 
 exec "$@"
