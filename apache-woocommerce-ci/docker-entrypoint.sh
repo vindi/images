42278fb6151379b4044aa48d9476cb03d2785cd4
Rubens S. Takauti <rtakauti@hotmail.com>
2018-10-19 10:49:14 -0300

    Ajusta Dockerfile

--- apache-woocommerce-ci/docker-entrypoint.sh
+++ apache-woocommerce-ci/docker-entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git pull
+composer update
 /wait-for-it.sh --timeout=10 wordpress_db:3306 --timeout=10 -- echo "mysql is loaded"
 wp plugin update --all --allow-root
 wp plugin activate vindi-woocommerce-subscriptions --allow-root
